e3dba07da18e6c2513ca41363969ea2c626027fb
Stenio Silveira <stenioas@gmail.com>
2020-12-10 15:53:55 -0300

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -917,14 +917,31 @@ _print_title_alert() {
   T_TITLE=$(echo ${#1})
   T_REST_COLS=$(( T_COLS - T_TITLE - 4 ))
   #printf "${RED}%$(( T_TITLE + 2 ))s${RESET}"|tr ' ' '_'
-   echo -ne "${BGREEN}╔"; echo -ne "`seq -s '═' $(( T_COLS - T_APP_TITLE - 4)) | tr -d [:digit:]`"; echo -ne "${BBLUE} ${APP_TITLE} ${RESET}"; echo -e "${BGREEN}╗${RESET}"
-  #tput cuf $(( T_COLS - T_TITLE - T_APP_TITLE - 3 )); echo -e "${BBLUE}${APP_TITLE}${RESET}"
+  echo -ne "${BGREEN}╔"; echo -ne "`seq -s '═' $(( T_TITLE + 2 )) | tr -d [:digit:]`"; echo -e "${BGREEN}╗${RESET}"
+  #echo -ne "${BBLUE} ${APP_TITLE} ${RESET}"
+  tput cuf $(( T_COLS - T_TITLE - T_APP_TITLE - 5 )); echo -e "${BBLUE}${APP_TITLE}${RESET}"
   #echo -ne "${BGREEN}╔"; echo -ne "`seq -s '═' 127 | tr -d [:digit:]`"; echo -e "╗${RESET}"
   echo -ne "${BGREEN}║${RESET}"; echo -ne "${BRED} $1 ${RESET}"; echo -e "`seq -s ' ' $(( T_COLS - T_TITLE - 4)) | tr -d [:digit:]`"; echo -e "${BGREEN}║${RESET}"
   echo -ne "${BGREEN}╚"; echo -ne "`seq -s '═' $(( T_COLS - 2 )) | tr -d [:digit:]`"; echo -e "╝${RESET}"
   #printf "${RED}%${T_REST_COLS}s\n${RESET}"|tr ' ' '-'
   #printf "${RED}%$(( T_TITLE + 2 ))s${RESET}"|tr ' ' '"'
 }
+
+#_print_title_alert() {
+#  clear
+#  T_COLS=$(tput cols)
+#  T_APP_TITLE=$(echo ${#APP_TITLE})
+#  T_TITLE=$(echo ${#1})
+#  T_REST_COLS=$(( T_COLS - T_TITLE - 4 ))
+#  #printf "${RED}%$(( T_TITLE + 2 ))s${RESET}"|tr ' ' '_'
+#   echo -ne "${BGREEN}╔"; echo -ne "`seq -s '═' $(( T_COLS - T_APP_TITLE - 4)) | tr -d [:digit:]`"; echo -ne "${BBLUE} ${APP_TITLE} ${RESET}"; echo #-e "${BGREEN}╗${RESET}"
+#  #tput cuf $(( T_COLS - T_TITLE - T_APP_TITLE - 3 )); echo -e "${BBLUE}${APP_TITLE}${RESET}"
+#  #echo -ne "${BGREEN}╔"; echo -ne "`seq -s '═' 127 | tr -d [:digit:]`"; echo -e "╗${RESET}"
+#  echo -ne "${BGREEN}║${RESET}"; echo -ne "${BRED} $1 ${RESET}"; echo -e "`seq -s ' ' $(( T_COLS - T_TITLE - 4)) | tr -d [:digit:]`"; echo -e "$#{BGREEN}║${RESET}"
+#  echo -ne "${BGREEN}╚"; echo -ne "`seq -s '═' $(( T_COLS - 2 )) | tr -d [:digit:]`"; echo -e "╝${RESET}"
+#  #printf "${RED}%${T_REST_COLS}s\n${RESET}"|tr ' ' '-'
+#  #printf "${RED}%$(( T_TITLE + 2 ))s${RESET}"|tr ' ' '"'
+#}
 
 _print_info() {
   T_COLS=$(tput cols)
@@ -956,7 +973,7 @@ _print_danger() {
 _print_done() {
   T_COLS=$(tput cols)
   echo ""
-  echo -e "${BG_GREEN}${BWHITE}[ DONE ]${RESET}" | fold -sw $(( T_COLS - 1 ))
+  echo -e "${BWHITE}[ DONE ]${RESET}" | fold -sw $(( T_COLS - 1 ))
 }
 
 _pause_function() {
