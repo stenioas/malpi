dccaab60bda8365322d8b2b9123045d8298214c6
Stenio Silveira <stenioas@gmail.com>
2020-12-10 15:27:49 -0300

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -918,7 +918,7 @@ _print_title_alert() {
   T_REST_COLS=$(( T_COLS - T_TITLE - 4 ))
   printf "${RED}%$(( T_TITLE + 2 ))s${RESET}"|tr ' ' '_'
   tput cuf $(( T_COLS - T_TITLE - T_APP_TITLE - 3 )); echo -e "${BBLUE}${APP_TITLE}${RESET}"
-  echo -e "${BGREEN}`seq -s '#' 129 | tr -d [:digit:]`${RESET}"
+  echo -e "${BGREEN}`seq -s '═' 129 | tr -d [:digit:]`${RESET}"
   echo -ne "${BG_RED}${BYELLOW}║ $1 ${RESET}${RED}║=${RESET}"
   printf "${RED}%${T_REST_COLS}s\n${RESET}"|tr ' ' '-'
   printf "${RED}%$(( T_TITLE + 2 ))s${RESET}"|tr ' ' '"'
