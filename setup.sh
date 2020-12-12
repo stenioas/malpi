54aaebda8e701a587926685173e5123b5a576e2b
Stenio Silveira <stenioas@gmail.com>
2020-12-12 16:59:02 -0300

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -829,12 +829,11 @@ _print_title() {
   clear
   T_COLS=$(tput cols)
   T_APP_TITLE=$(echo ${#APP_TITLE})
+  T_TITLE=$(echo ${#1})
   T_LEFT="${YELLOW}░▒▓${RESET}${BG_YELLOW}${BWHITE} $1${RESET}"
   T_RIGHT="${BG_YELLOW}${BLACK}${APP_TITLE}${RESET}${YELLOW}▓▒░${RESET}"
-  T_COLS_LEFT=$(( ${#1} + 4 ))
-  T_COLS_RIGHT=$(( ${#T_APP_TITLE} + 3 ))
   echo -ne "${T_LEFT}"
-  echo -ne "${BG_YELLOW}${YELLOW}`seq -s ' ' $(( T_COLS - T_COLS_LEFT - T_COLS_RIGHT )) | tr -d [:digit:]`${RESET}"
+  echo -ne "${BG_YELLOW}${YELLOW}`seq -s ' ' $(( T_COLS - T_TITLE - T_APP_TITLE )) | tr -d [:digit:]`${RESET}"
   echo -e "${T_RIGHT}"
 }
 
