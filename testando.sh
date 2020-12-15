575acef0fd98627bffac660c96e949444515ed83
Stenio Silveira <stenioas@gmail.com>
2020-12-15 20:26:19 -0300

    Add files via upload

--- testando.sh
+++ testando.sh
@@ -1,34 +1,17 @@
-_package_install() {
-  _package_was_installed() {
-    for PKG in $1; do
-      if [[ $(id -u) == 0 ]]; then
-        pacman -S --noconfirm --needed "${PKG}" 1> /dev/null && return 0;
-      else
-        sudo pacman -S --noconfirm --needed "${PKG}" 1> /dev/null && return 0;
-      fi
-    done
-    return 1
-  }
-  #install packages using pacman
-  for PKG in $1; do
-    if ! _is_package_installed "${PKG}"; then
-      echo -ne " ${BBlue}Installing${Reset} ${BCyan}[ ${PKG} ]${Reset} ..."
-      if _package_was_installed "${PKG}"; then
-        echo -e " ${BYellow}[ SUCCESS! ]"
-      else
-        echo -e " ${BRed}[ ERROR! ]"
-      fi
-    else
-      echo -e " ${BBlue}Installing${Reset} ${BCyan}[ ${PKG} ]${Reset} ... ${Yellow}[ It's already installed. ]${Reset}"
-    fi
-  done
-}
+#!/bin/sh
 
-_is_package_installed() {
-  for PKG in $1; do
-    pacman -Q "$PKG" &> /dev/null && return 0;
-  done
-  return 1
-}
-
-_package_install "neofetch nano vim testando"
+printf "%s                    -@"
+printf "%s                   .##@"
+printf "%s                  .####@"
+printf "%s                  @#####@"
+printf "%s                . *######@"
+printf "%s               .##@o@#####@"
+printf "%s              /############@"
+printf "%s             /##############@"
+printf "%s            @######@**%######@"
+printf "%s           @######`     %#####o"
+printf "%s          @######@       ######%"
+printf "%s        -@#######h       ######@.`"
+printf "%s       /#####h**``       `**%@####@"
+printf "%s      @H@*`                    `*%#@"
+printf "%s     *`                            `*"
