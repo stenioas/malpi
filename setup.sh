c85ee4a505133cb3ef4b4e4c612d32795d16e24e
Stenio Silveira <stenioas@gmail.com>
2020-12-22 23:52:05 -0300

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -880,12 +880,12 @@ _print_title() {
   BORDER_COLOR=${WHITE}
   T_APP_TITLE=${#APP_TITLE}
   T_TITLE=${#1}
-  T_LEFT="${BORDER_COLOR}║${RESET}${BG_WHITE}${BLACK}   $1   ${RESET}${BORDER_COLOR}║${RESET}"
+  T_LEFT="${BORDER_COLOR}──╢${RESET}${BG_WHITE}${BLACK}   $1   ${RESET}${BORDER_COLOR}╟${RESET}"
   T_RIGHT="${BBLACK} ${APP_TITLE}${RESET}"
   echo -ne "${BORDER_COLOR}`seq -s ' ' $(( T_COLS - T_APP_TITLE - 1 )) | tr -d [:digit:]`${RESET}"
   echo -e "${T_RIGHT}"
   echo -ne "${T_LEFT}"
-  echo -e "${BORDER_COLOR}`seq -s '═' $(( T_COLS - T_TITLE - 8 )) | tr -d [:digit:]`${RESET}"
+  echo -e "${BORDER_COLOR}`seq -s '═' $(( T_COLS - T_TITLE - 9 )) | tr -d [:digit:]`${RESET}"
 }
 
 _print_title_alert() {
