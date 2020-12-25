bf8878db435fc378ed909ef5b7ca8ca909653330
Stenio Silveira <stenioas@gmail.com>
2020-12-25 19:01:27 -0300

    Add files via upload

--- test.sh
+++ test.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-echo -e "Installing base --------------------------------------------------------------------------- [ FAIL ]"
+_set_localization() {
+	ITEMS=$(find /usr/share/kbd/keymaps/ -type f -printf "%f" | sort -V)
+  OPTIONS=("TESTE" "TESTE2" "TESTE3")
+	#KEYMAP_LIST=()
+	#for ITEM in ${ITEMS}; do
+	#	KEYMAP_LIST+=("${ITEM%%.*}")
+	#done
+  echo "${ITEMS}"
+  echo ${ITEMS}
+  echo "${OPTIONS}"
+  echo ${OPTIONS}
+}
+
+_set_localization
