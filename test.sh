e3feaad4849eeb7b4c19c5e9f0300f7af83e88b4
Stenio Silveira <stenioas@gmail.com>
2020-12-25 19:15:59 -0300

    Add files via upload

--- test.sh
+++ test.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
 _set_localization() {
-	ITEMS=$(find /usr/share/kbd/keymaps/ -type f -printf "%f" | sort -V)
-  OPTIONS=("TESTE" "TESTE2" "TESTE3")
-	#KEYMAP_LIST=()
-	#for ITEM in ${ITEMS}; do
-	#	KEYMAP_LIST+=("${ITEM%%.*}")
-	#done
-  echo "${ITEMS}"
-  echo ${ITEMS}
-  echo "${OPTIONS}"
-  echo ${OPTIONS}
+	ITEMS=$(find /usr/share/kbd/keymaps/ -type f -printf "%f\n" | sort -V)
+	KEYMAP_LIST=()
+	for ITEM in ${ITEMS}; do
+		KEYMAP_LIST+=("${ITEM%%.*}")
+	done
+  select KEYMAP_CHOICE in "${KEYMAP_LIST[@]}"; do
+    if _contains_element "${KEYMAP_CHOICE}" "${KEYMAP_LIST[@]}"; then
+      KEYMAP_CHOICE="${KEYMAP_CHOICE}"
+      break;
+    else
+      echo "Opcao errada"
+    fi
+  done  
 }
 
 _set_localization
