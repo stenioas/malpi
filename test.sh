7b63274f3f04b0d68df1b64a9543e87ff267db50
Stenio Silveira <stenioas@gmail.com>
2021-01-01 14:16:35 -0300

    Add files via upload

--- test.sh
+++ test.sh
@@ -1,7 +1,11 @@
 #!/bin/usr/env sh
 
+_is_luks_volumes_open() {
+  local LUKS_VOLUMES=$(lsblk --fs | awk '{print $2}' | grep crypto_LUKS)
+  [[ ! "$LUKS_VOLUMES" = "" ]] && return 0 || return 1;
+}
 LUKS_OPEN=$(dmsetup -ls --target crypt | awk '{print $1}')
-if [[ ! "$LUKS_OPEN" = "" ]]; then
+if ! _is_luks_volumes_open; then
   for ITEM in ${LUKS_OPEN[@]}; do
     cryptsetup close /dev/mapper/${ITEM}
   done
