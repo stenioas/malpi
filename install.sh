aaf73e8fc729b565b54a6172b8b98f1fe0da7031
stenioas <stenioas@gmail.com>
2022-08-15 20:09:02 -0300

    refactor: modulariza o projeto

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# ============================================================================
+# Script   : install.sh
+# Descrição: Script de instalação do Archlinux
+# Versão   : 0.0.1
+# Autor    : Stenio Silveira <stenioas@gmail.com>
+# Data     : 13/08/2022
+# Licença  : GNU/GPL v3.0
+# ----------------------------------------------------------------------------
+
+# ============================================================================
+# DEPENDENCIES
+# ----------------------------------------------------------------------------
+
+[[ -e ./lib/functions ]] && source ./lib/functions || { echo "O arquivo 'functions' está faltando."; exit 1; }
+[[ -e ./install.conf ]] && source ./install.conf || { echo "O arquivo 'install.conf' está faltando."; exit 1; }
+
+# ============================================================================
+# TESTS
+# ----------------------------------------------------------------------------
+
+_check_archlinux
+_check_root
+_check_uefimode
+_check_connection
+_check_pacman_blocked
+_set_font
+_pause_function
+
+# ============================================================================
+# EXECUTION
+# ----------------------------------------------------------------------------
+
+_select_disk
+_format_partitions
+_rank_mirrors
+_install_base
+_generate_fstab
+_set_timezone
+_set_locale
+_set_network
+_generate_mkinitcpio
+_set_bootloader
+_set_root_passwd
+_install_finish
